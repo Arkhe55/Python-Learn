c50b788ddc7f718148251081a9de2998963aa17b
Arkhe55 <gabriel_4682@outlook.com>
2024-07-09 13:28:14 -0300

    python-learn

---
+++ Lógica de programação/aula2.py
@@ -0,0 +1,8 @@
+# Argumentos não nomeados
+# Sep="" Separador
+# \r\n -> CRLF Carriage Return Line Feed
+# \n -> LF Line Feed
+print(12, 54, sep="|", end="\r\n")
+print(45, 78, sep="|", end="\r\n")
+print(5, 88, sep="|", end="###\n")
+print(15, 43, sep="|", end="###")
