b3c3493834e2fcddfcc0b17d7268f6d8e2bafadc
Arkhe55 <gabriel_4682@outlook.com>
2024-07-10 14:30:42 -0300

    Python-learn

---
+++ Lógica de programação/aula05.py
@@ -0,0 +1,13 @@
+"""Tipos de dados bool (bolean)
+ao questionar algo em um programa
+só existe duas respostas possiveis
+True:Sim ou False:Não
+Existe varios operadores para "questionar".
+dentre eles o ==, é o operador lógico
+que questiona se um valor é igual a outro."""
+
+print("10 é igual a 10?:", 10 == 10) #Sim ou True (Verdadeiro)
+print("10 é igual a 9? :", 10 == 9) #Não ou False (Falso)
+
+print(1, type(10 == 10), sep="º ")
+print(2, type(10 == 9), sep="º ")
