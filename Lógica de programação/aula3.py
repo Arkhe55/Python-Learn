11ff8437049d3046628d813172728466d86a5e50
Arkhe55 <gabriel_4682@outlook.com>
2024-07-10 13:31:33 -0300

    Python learn

---
+++ Lógica de programação/aula3.py
@@ -0,0 +1,17 @@
+"""
+DocString
+Python: Linguagem de programação
+Tipo de tipagem: Dinamica / Forte
+str -> String -> Texto
+
+"""
+
+#Escape
+print(1, "Gabriel \"Santos\" \"Silva\"")
+
+#r
+print(2, r"Gabriel \"Santos\" \"Silva\"")
+
+print(3, 'Gabriel Santos "Silva"')
+
+
