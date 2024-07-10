ef8ca1fcdebd09d7db157a768400cfa8b812f2d4
Arkhe55 <gabriel_4682@outlook.com>
2024-07-10 14:06:11 -0300

    python-learn

---
+++ Lógica de programação/aula4.py
@@ -0,0 +1,24 @@
+#Tipos int e float
+#Int -> Números inteiros
+#Tipo int representa qualquer número
+#possitivo ou negativo, int sem sinal é considerado possitivo
+
+print(1, 50, sep="º ")
+print(2, -50, sep="º ")
+print(3, 0, sep="º ")
+
+#float -> Números com ponto flutuante
+#O tipo float representa qualuqer número
+#possitivo negativo com ponto flutuante
+#float sem sinal é considerado possitivo
+
+print(4, 5.3, sep="º ")
+print(5, -5.23, sep="º ")
+print(6, 0.23, sep="º ")
+
+#A função type mostra o tipo que o Python
+#inseriu ao valor
+
+print(type(5))
+print(type(0.5))
+print(type("Gabriel"))
