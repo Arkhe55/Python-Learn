860980a2c2d8024c2c855eb39e0c3b3ffc1bfc94
Arkhe55 <gabriel_4682@outlook.com>
2024-07-11 12:30:20 -0300

    Python-learn

---
+++ Lógica de programação/aula06.py
@@ -0,0 +1,10 @@
+#conversão de tipos, coerção
+#type convertion, typecasting, coercion
+#é o ato de converter um tipo de dado em outro
+#tipo imutaveis e primitivos:
+#str, int, float, bool
+print('a' + 'b')
+print(int('1'), type(int('1'))) #conversão de tipo
+print(int('1') + 1)
+print(type(int('1') + 2))
+print(str(11) + 'B')
