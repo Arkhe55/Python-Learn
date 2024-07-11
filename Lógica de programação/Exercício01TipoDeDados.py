d976beba5836ede734ae2ff2e0094d597add1412
Arkhe55 <gabriel_4682@outlook.com>
2024-07-11 13:20:42 -0300

    Python_learn

---
+++ Lógica de programação/Exercício01TipoDeDados.py
@@ -0,0 +1,14 @@
+nome = 'Gabriel'
+sobrenome = 'Santos silva'
+idade = 26
+ano_nascimento = (1997)
+altura = 1.77
+maior_idade = idade >= 18
+
+print('Nome:',nome) # Tipo str
+print('Sobrenome:',sobrenome) # Tipo str
+print('idade:',idade) # Tipo int
+print('Data de Nascimento:',ano_nascimento) # Tipo int
+print('Altura:',altura) # Tipo float
+print('Maior de idade?:', maior_idade) # Tipo bool
+
