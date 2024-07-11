00c19cc59937b30e66bdf6099eaefe7d81df958b
Arkhe55 <gabriel_4682@outlook.com>
2024-07-11 13:01:30 -0300

    Python_Learn

---
+++ Lógica de programação/aula07.py
@@ -0,0 +1,13 @@
+#Variáveis são usadas para salvar algo na memoria do computador
+#PEP8: inicie variáveis com letras minusculas, pode usar
+#números e underline _ 
+#O sinal de = é um atribuidor
+#Ele é usado para atribuir um valor a um nome (variavel)
+#Uso nome_variavel = expressão
+
+nome_completo = 'Gabriel santos silva'
+idade = 26
+cep = 'CEP:06784292'
+maior_de_idade = idade >= 18
+print('Nome completo:',nome_completo, 'idade:',idade, 'CEP:',cep)
+print('È maior de idade?: ',maior_de_idade)
