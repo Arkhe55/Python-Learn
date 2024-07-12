cea8d96cf60873b304891d43888d534bc0c90b3a
Arkhe55 <gabriel_4682@outlook.com>
2024-07-12 13:25:10 -0300

    Python_learn

---
+++ Lógica de programação/aula8.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('adicao', adicao)
+
+subtracao = 10 - 5
+print('subtracao', subtracao)
+
+multiplicacao = 10 * 10
+print('Multipricação', multiplicacao)
+
+divisao = 10 / 2.2 # Sempre retorna float
+print('Divisão',divisao)
+
+divisao_inteira = 10 // 2
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 10 ** 10
+print('Exponenciação', exponenciacao)
+
+resto_divisao = 20 % 2
+print('Resto Divisão', resto_divisao)
