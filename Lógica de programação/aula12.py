89c18b7f3ef57be095e10d9270fb54e8830f3005
Arkhe55 <gabriel_4682@outlook.com>
2024-07-20 13:47:20 -0300

    Python_learn

---
+++ Lógica de programação/aula12.py
@@ -0,0 +1,10 @@
+# Função Input
+
+# nome = input("Qual seu nome? ")
+# print(f"O seu nome é {nome}")
+
+#Jeito errado de se classificar um dado
+numero_1 = int(input("Digite um número"))
+numero_2 = int(input("Digite outro número"))
+
+print(f"A Soma dos números é: {numero_1 + numero_2}")
