eb6827c0db5d5990170c14dcce8f720be4e1908b
Arkhe55 <gabriel_4682@outlook.com>
2024-07-22 16:09:04 -0300

    Python_learn

---
+++ Lógica de programação/aula14.py
@@ -0,0 +1,11 @@
+#Debuging Test
+condicao_1 = False
+condicao_2 = True
+condicao_3 = True
+
+if condicao_1:
+    print("Verdadeiro")
+elif condicao_2:
+    print("A Condicao_1 Não foi cumprida")
+elif condicao_3:
+    print("A Condicao_1 Não foi cumprida")
