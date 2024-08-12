4534fb82d44aa8c005f2f86eef181cc3be6ba9c6
Arkhe55 <gabriel_4682@outlook.com>
2024-08-12 20:59:54 -0300

    Python_learn

---
+++ Lógica de programação/Comparacao_de_valor.py
@@ -0,0 +1,7 @@
+primeiro_valor = input("Digite um valor: ")
+segundo_valor = input("Digite outro valor: ")
+if primeiro_valor > segundo_valor:
+    print(f'{primeiro_valor:} È maior ou igual' f'{segundo_valor}')
+else:
+    print(f'{segundo_valor} È maior ou igual ' f'{primeiro_valor}')
+
