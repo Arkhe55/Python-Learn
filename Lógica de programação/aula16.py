0790e21a26ffe8f05d1e91546458ec5bdca16e7c
Arkhe55 <gabriel_4682@outlook.com>
2024-08-12 22:01:04 -0300

    Python_learn

---
+++ Lógica de programação/aula16.py
@@ -0,0 +1,22 @@
+#Operadores lógicos
+#and (e) or (ou) not (não)
+#and: Todas as condiçoes precisam ser
+#verdadeiras.
+#se qualquer valor for considerado falso
+#a expressão inteira será avaliada naquele valor
+#São considerados falsy: 0 0.0 "" False
+#Também tem o tipo none que é
+#considerado não valor
+
+# entrada = input("[E]ntrada ou [S]aida: ")
+
+# senha_permitida = "123456"
+# senha_digitada = input("Digite a senha: ")
+
+# if entrada == "E" and senha_permitida == senha_digitada:
+#     print("Entrou")
+# else:
+#     print("Saida")
+
+if 0 and 1:
+    print(True and 1)
