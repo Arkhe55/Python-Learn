be5fe36f4dce891f925b7db59c8ecca90dabe9ce
Arkhe55 <gabriel_4682@outlook.com>
2024-08-14 01:19:43 -0300

    Python_learn

--- Lógica de programação/aula16.py
+++ Lógica de programação/aula16.py
@@ -2,21 +2,18 @@
 #and (e) or (ou) not (não)
 #and: Todas as condiçoes precisam ser
 #verdadeiras.
-#se qualquer valor for considerado falso
+#se qualquer valor for considerado falso ou verdadeiro
 #a expressão inteira será avaliada naquele valor
 #São considerados falsy: 0 0.0 "" False
 #Também tem o tipo none que é
 #considerado não valor
 
-# entrada = input("[E]ntrada ou [S]aida: ")
+entrada = input("[E]ntrada ou [S]aida: ")
 
-# senha_permitida = "123456"
-# senha_digitada = input("Digite a senha: ")
+senha_permitida = "123456"
+senha_digitada = input("Digite a senha: ")
 
-# if entrada == "E" and senha_permitida == senha_digitada:
-#     print("Entrou")
-# else:
-#     print("Saida")
-
-if 0 and 1:
-    print(True and 1)
+if (entrada == "E" or entrada == "e") and senha_permitida == senha_digitada:
+    print("Entrou")
+else:
+    print("Saida")
