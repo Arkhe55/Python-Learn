0def9880ec22a4cdfca48e994f02a3bf8d359327
Arkhe55 <gabriel_4682@outlook.com>
2024-08-14 02:15:30 -0300

    Python_learn

---
+++ aula17.py
@@ -0,0 +1,15 @@
+#operador lógico "not"
+#Usado para inverter expressões
+#not True = False
+#not False = True
+
+#print(not False)
+
+senha = input("Digite uma senha")
+
+#if senha != "123456":
+    #print("Senha incorreta")
+
+if not senha:
+    print("Você não digitou a senha")
+
