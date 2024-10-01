f4c78ab5848a8bb7300abecce48fde4993b1bf30
Arkhe55 <gabriel_4682@outlook.com>
2024-10-01 14:53:41 -0300

    Python-learn

---
+++ Lógica de programação/aula19.py
@@ -0,0 +1,10 @@
+#interpolação
+#x e X Hexadecimal (0123456789ABCDEF)
+
+nome = "Gabriel"
+preco = 1.500
+
+saldo= '%s seu saldo devedor é R$%.3f' % (nome, preco)
+
+print(saldo)
+print("O hexadecimal de %i é %.04X" % (13, 13))
