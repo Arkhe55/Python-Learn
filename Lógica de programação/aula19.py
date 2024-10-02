3aae9ecb0faf36769f7d5e7ce26e53c0552456c3
Arkhe55 <gabriel_4682@outlook.com>
2024-10-02 16:16:35 -0300

    Python_learn

--- Lógica de programação/aula19.py
+++ Lógica de programação/aula19.py
@@ -8,3 +8,13 @@ saldo= '%s seu saldo devedor é R$%.3f' % (nome, preco)
 
 print(saldo)
 print("O hexadecimal de %i é %.04X" % (13, 13))
+
+#F String
+
+variavel='ABC'
+print(f'{variavel:a>10}')
+print(f'{variavel:a^11}')
+print(f"{1503.2556:0<+5.1f}")
+numero=12500
+print(f'{numero:,}')
+
