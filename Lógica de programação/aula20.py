105b51274d1907a22b0155cd82e858b66dcfe745
Arkhe55 <gabriel_4682@outlook.com>
2024-10-04 20:03:06 -0300

    Python-learn

---
+++ Lógica de programação/aula20.py
@@ -0,0 +1,14 @@
+"""Fatiamento de string
+indice[] Possitivo 012345678
+Negativo -987654321 = Olá Mundo (Espaço também é contado)
+Fatiamento [i:f:p] [::]
+Função len de lenght de comprimento denomina quantidade de uma string
+"""
+
+variavel=("Olá Mundo")
+
+print(variavel[4:])
+print(variavel[-6:])
+print(variavel[0:9:1])
+print(variavel[-1:-10:-1])
+print(len(variavel))
