d9311143da89014d47f100b106ea44187afc2d52
Arkhe55 <gabriel_4682@outlook.com>
2024-10-07 17:18:06 -0300

    Python-learn

---
+++ Python/aula22.py
@@ -0,0 +1,19 @@
+variavel = "a"
+print(id(variavel))
+
+condicao = True
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print("Executado")
+
+else:
+    print("Não executado")
+
+print(passou_no_if, passou_no_if is None)
+print(passou_no_if, passou_no_if is not None)
+
+
+
+
