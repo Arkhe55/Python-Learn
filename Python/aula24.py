4a0c629404c33a775223ccafef302acfc033c55a
Arkhe55 <gabriel_4682@outlook.com>
2024-10-18 19:12:27 -0300

    Python-learn

---
+++ Python/aula24.py
@@ -0,0 +1,17 @@
+quant_linhas = 5
+quant_colunas = 5
+
+linhas = 1
+while quant_linhas >= linhas:
+    coluna = 1
+    while quant_colunas >= coluna:
+        print(f"{linhas=} {coluna=}")
+        coluna += 1
+    linhas += 1
+
+
+    
+    # while quant_colunas >= linhas:
+    #     print(linhas, coluna)
+    #     coluna += 1
+    # linhas += 1
