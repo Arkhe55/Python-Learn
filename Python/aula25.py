adbc4e0cddff734d8beefd97c22d084568173c85
Arkhe55 <gabriel_4682@outlook.com>
2025-04-03 11:30:33 -0300

    Python-learn

---
+++ Python/aula25.py
@@ -0,0 +1,14 @@
+# texto = "Python"
+
+# i = 0
+
+# tamanho_texto = len(texto)
+
+# while i < tamanho_texto:
+#     print(texto[i])
+#     i += 1
+
+texto = "Python"
+
+for letra in texto:
+    print(letra)
