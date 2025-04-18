87afc5e26a43295a2a407ef457fc69793846f59e
Arkhe55 <gabriel_4682@outlook.com>
2025-04-18 13:39:04 -0300

    Python-learn

---
+++ Python/indice-exercicio.py
@@ -0,0 +1,13 @@
+# lista = ["Gabriel", "Rafael", "Miguel"]
+# lista.append("Rodolfo")
+
+# i = range(len(lista))
+
+# for indice in i:
+#     print(indice, lista[indice])
+
+lista = ["Gabriel", "Rafael", "Miguel"]
+
+for indice in range(len(lista)):
+    print(f"{indice}: {lista[indice]}")
+
