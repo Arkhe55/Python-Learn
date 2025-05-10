72b094373a05241d9ff4a33c4dc63b3c9f349a20
Arkhe55 <gabriel_4682@outlook.com>
2025-05-10 19:45:31 -0300

    Python_learn

---
+++ Python/aula36.py
@@ -0,0 +1,11 @@
+"""Tipo tuplas uma lista imutável"""
+
+#Tuple
+nome = "Gabriel", "Rafael", "Luiz"
+print(nome)
+
+#Conversão list para tuple
+numero = [1, 2 ,3, 4, 5]
+
+numero = tuple(numero)
+print(numero)
---
+++ Python/aula37.py
@@ -0,0 +1,9 @@
+"""Enumerate - enumerate iterável (indice)"""
+
+lista = ["Gabriel", "Luiz", "Ronaldo"]
+lista_enumerada = enumerate(lista)
+
+# print(next(lista_enumerada))
+
+for item in lista_enumerada:
+     print(item)
