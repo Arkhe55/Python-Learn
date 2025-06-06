92d4f6ad98c830d99d8032ad3dabbd304d2ee7a7
Arkhe55 <gabriel_4682@outlook.com>
2025-06-05 21:18:00 -0300

    Python-learn

--- Python/aula38.py
+++ Python/aula38.py
@@ -4,8 +4,18 @@ Double-precision floating-point format IEEE 754
 https://en.wikipedia.org/wiki/Double-precision_floating-point_format
 https://docs.python.org/pt-br/3/tutorial/floatingpoint.html
 """
+
+#   "Para programas que nescecitam de precisão
+#           em com números decimais "
+# import decimal
+# numero_1 = decimal.Decimal('0.1')
+# numero_2 = decimal.Decimal('0.7')
+
 numero_1 = 0.1
 numero_2 = 0.7
 soma = numero_1 + numero_2
 
-print(soma)
+
+print(type (soma))
+print(type (f"{soma:.1f}"))
+print(type (round(soma, 2)))
