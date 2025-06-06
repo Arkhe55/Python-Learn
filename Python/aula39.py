2ea1c8e135eb1dc1936dffc3d30d1827b78fb1ca
Arkhe55 <gabriel_4682@outlook.com>
2025-06-06 12:02:08 -0300

    Python-learn

--- Python/aula39.py
+++ Python/aula39.py
@@ -1,4 +1,5 @@
-""" Split """
+""" Split divide uma string 
+    Join Une uma string """
 
 frase = "É mais importante dominar" \
 " as cartas que você tem nas mãos do que reclamar " \
@@ -9,6 +10,9 @@ lista_de_frases = frase.split()
 # for indice, frase in enumerate(lista_de_frases):
 #     print (lista_de_frases[indice])
 
-for indice, frase in enumerate(lista_de_frases):
-    print(lista_de_frases[indice])
+for indice in enumerate(lista_de_frases):
+    print(f"{indice}\n")
 
+print(" --- Join --- \n ")
+frases_unidas = "-".join("abc")
+print(frases_unidas)
