0be85fdb2ebbd87feeff6e9e774068343dce514d
Arkhe55 <gabriel_4682@outlook.com>
2025-06-05 22:23:12 -0300

    Python_learn

---
+++ Python/aula39.py
@@ -0,0 +1,14 @@
+""" Split """
+
+frase = "É mais importante dominar" \
+" as cartas que você tem nas mãos do que reclamar " \
+" das cartas que seu oponente recebeu"
+
+lista_de_frases = frase.split()
+
+# for indice, frase in enumerate(lista_de_frases):
+#     print (lista_de_frases[indice])
+
+for indice, frase in enumerate(lista_de_frases):
+    print(lista_de_frases[indice])
+
