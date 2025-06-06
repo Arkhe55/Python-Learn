0ec80ac87b270a91951fc2108d156d7ff11b4658
Arkhe55 <gabriel_4682@outlook.com>
2025-06-06 13:44:07 -0300

    Python-learn

---
+++ Python/aula40.py
@@ -0,0 +1,13 @@
+salas = [
+    ["Maria", "João"],
+
+    ["Flamengo", "Steve"],
+
+    ["Leonardo", "Luiz",] #(0, 10, 20, 30, 40)]
+]
+
+# print(salas[2][2][2])
+
+for sala in salas:
+    for aluno in sala:
+        print(aluno)
