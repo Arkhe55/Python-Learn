7b835070c84612dc70c1baea0ba293cd0b981160
Arkhe55 <gabriel_4682@outlook.com>
2025-06-07 13:21:44 -0300

    Python-learn

---
+++ Python/aula41.py
@@ -0,0 +1,15 @@
+#desempacotamento em chamadas
+#de método e funções
+string = "ABC"
+lista = ["João", "Pedro","Matheus", 1, 2, 3, "eduardo"]
+tupla = 'Python', 'Jardim', 'Bloodborner2'
+
+# a, b, c, *_ = lista
+
+print(*lista)
+print(*string)
+print(*tupla)
+
+# for itens in enumerate(tupla):
+#     print(itens)
+
---
+++ Python/aula42.py
@@ -0,0 +1,15 @@
+"""Operação ternaria (condicional de uma linha)
+<valor> if <condição> else <outro valor>"""
+
+# print('Valor' if False else "outro valor")
+
+# condicao = 10 == 11
+# variavel = 'Valor' if condicao else "Outro Valor"
+# print(variavel)
+
+# digito = 10 # > 9 = 0
+# # novo_digito = digito if digito <= 9 else 0
+# novo_digito = 0 if digito > 9 else digito
+# print(novo_digito)
+
+print("Valor" if True else "Outro Valor" if False else "Fim")
