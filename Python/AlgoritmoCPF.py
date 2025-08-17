9aa8fd5dce0c3534862d8dd2c097b68c2c1783f2
Arkhe55 <gabriel_4682@outlook.com>
2025-08-17 10:07:26 -0300

    Python-learn

--- Python/AlgoritmoCPF.py
+++ Python/AlgoritmoCPF.py
@@ -31,18 +31,20 @@ while True:
     print(dado_usuario)
 
     try:
-        if len(dado_usuario) != 11:
+        if len(dado_usuario) != 9:
             raise Exception("")
-        nove_digitos = dado_usuario[:9]
+        dez_digitos = dado_usuario
         contador_regresivo_1 = 10
         resultado_1 = 0
-        for indice_1 in nove_digitos:
+        for indice_1 in dez_digitos:
             resultado_1 += int(indice_1) * contador_regresivo_1
             contador_regresivo_1 -= 1
         resultado_nove_digitos = (resultado_1 * 10) % 11
         resultado_nove_digitos = resultado_nove_digitos if resultado_nove_digitos <= 9 else 0
         print(f"-- Primeiro digito dos 2 Ultimos: {resultado_nove_digitos} --")
-        dez_digitos = dado_usuario[:10]
+        
+        # Segundo digito dos 2 Ultimos
+        dez_digitos = dado_usuario + str(resultado_nove_digitos)
         contador_regresivo_2 = 11
         resultado_2 = 0
         for indice_2 in dez_digitos:
@@ -55,48 +57,6 @@ while True:
     except Exception as erro:
         print(erro)
         print("Tivemos um problema. Verifique se seu cpf é valido Exemplo: 062.464.560-65")
-    
-    # resultado_1 = 0
-    # if len(dado_usuario) == 11:
-    #     nove_digitos = dado_usuario[:9]
-    #     contador_regresivo_1 = 10
-    #     for digito_1 in nove_digitos:
-    #             resultado_1 += int(digito_1) * contador_regresivo_1
-    #             digito_1 = (resultado_1 * 10) % 11
-    #             digito_1 = digito_1 if digito_1 <= 9 else 0
-    #             print(f"-- Primeiro Digito {digito_1} --")
-            
-        # resultado_1 += digito_1 * contador_regresivo_1
-        # contador_regresivo_1 -= 1
-
-        # digito_1 = (resultado_1 * 10) % 11
-        # digito_1 = digito_1 if digito_1 <= 9 else 0
-        # print(f"-- Primeiro Digito {digito_1} --")
-
-        # dez_digitos = dado_usuario []
-        # for digito_2 in dez_digitos:
-        #      print(digito_2)
-       
-
-    # else:
-    #     print("CPF invalido")
-
-
-
-    # try:
-    #     dado_usuario = []
-    #     dado_usuario.append(int(input("Digite seu CPF: ")))
-    # except ValueError:
-    #     print("Digite apenas números, por favor ^-^")
-    
-
-    
-
-    
-    # try:
-    #     # cpf.append(float (input("Digite seu CPF: ")))
-    # except ValueError:
-    #     print("Digite apenas números, por gentileza")
 
 
     
