76fb12021a677305b6247efd618457fb219c7a07
Arkhe55 <gabriel_4682@outlook.com>
2025-09-01 21:08:39 -0300

    Python

--- Intermediario/aaa.py
+++
@@ -1,6 +0,0 @@
-contagem = 10
-nome = "gabriel"
-for i in nome:
-    contagem -= 1
-    print(contagem)
-    
---
+++ Intermediario/aula44.py
@@ -0,0 +1,9 @@
+def soma(x, y):
+    print(f"{x=} {y=}", "|", "x + y = ", x + y)
+
+soma(1, 2)
+soma(y=5, x=5)
+
+
+
+    
