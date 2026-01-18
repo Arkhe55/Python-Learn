f2d74ad1b5cb027b051c09d85b5dba32806bd351
Arkhe55 <gabriel_4682@outlook.com>
2026-01-18 18:59:39 -0300

    Python-learn

--- Intermediario/aula44.py
+++ Intermediario/aula44.py
@@ -1,9 +1,6 @@
+#Definição da função
 def soma(x, y):
-    print(f"{x=} {y=}", "|", "x + y = ", x + y)
+    #print(f"{x=} {y=}", "|", "x + y = ", x + y)
+    print(x + y)
 
 soma(1, 2)
-soma(y=5, x=5)
-
-
-
-    
