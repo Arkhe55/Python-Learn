e5234a4286897ef0fecf1962e33eef4b981f5883
Arkhe55 <gabriel_4682@outlook.com>
2026-04-18 11:03:49 -0300

    Python-learn

---
+++ Intermediario/aula57.py
@@ -0,0 +1,12 @@
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.htm
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+
+# Criando um Set
+# set(iteravel) ou {1, 2, 3}
+
+v1 = set("Gabriel")
+print(v1)
