55125c230a19e2dfa4df935799d82f86a56756e7
Arkhe55 <gabriel_4682@outlook.com>
2026-04-19 10:58:46 -0300

    Python-learn

--- Intermediario/aula57.py
+++ Intermediario/aula57.py
@@ -8,5 +8,28 @@
 # Criando um Set
 # set(iteravel) ou {1, 2, 3}
 
-v1 = set("Gabriel")
-print(v1)
+# set_1 = set('Gabriel')
+# set_2 = {'Gabriel'}
+
+# print(type(set_2))
+
+# Sets são eficientes para remover valores duplicados
+# de iteráveis.
+# - Seus valores serão sempre únicos;
+# - Não aceitam valores mutáveis;
+# - não tem índexes;
+# - não garantem ordem;
+# - são iteráveis (for, in, not in)
+
+set_1 = {1, 2 ,3, 3, 3, 3, 3, 5, 4, 4, 1 ,4}
+
+print(set_1)
+
+# print(6 not in set_1)
+
+# set_1 = set(lista)
+# lista_2 = list(set_1)
+
+# for i, indice in enumerate(set_1):
+#     print(i, indice)
+
